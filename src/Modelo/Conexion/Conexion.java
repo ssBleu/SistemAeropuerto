e37b370e45294f14ad1d47da7fd14e50bde69985
Bleu <loayzajhosep58@gmail.com>
2023-06-26 16:38:24 -0500

    MySqlOnline

--- src/Modelo/Conexion/Conexion.java
+++ src/Modelo/Conexion/Conexion.java
@@ -1,19 +1,16 @@
 package Modelo.Conexion;
-import com.mysql.jdbc.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Conexion {
     
     public static Connection getConexion(){ 
 		Connection con = null;
 		try { Class.forName("com.mysql.jdbc.Driver"); 
-		String url = "jdbc:mysql://localhost/bd_vuelos"; 
-		String usr = "root";
-		String psw = ""; 
+		String url = "jdbc:mysql://sql10.freesqldatabase.com:3306/sql10628899"; //"jdbc:mysql://localhost/bd_vuelos" (opcion local)
+		String usr = "sql10628899"; //root (opcion local)
+		String psw = "YqmCP18gTM"; //nada (opcion local)
 		con = DriverManager.getConnection(url,usr,psw); 
 		System.out.println("conexion ok");
 		} catch (ClassNotFoundException ex)
